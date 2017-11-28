a75d28af7673dfce5f623fb2ef7421a974456856
gawadaz <gawad.azzam@gmail.com>
2017-11-29 00:57:44 +0200

    add test items

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
+  items: { desc: string, image:string }[] = [
+    { desc: "first item", image: "http://1.bp.blogspot.com/-H3xFUgDh8wU/Vj7h8O2xdqI/AAAAAAAAAh4/gk-gBPFf-wU/s1600/The%2BBox.jpg" },
+    { desc: "second item", image: "http://1.bp.blogspot.com/-H3xFUgDh8wU/Vj7h8O2xdqI/AAAAAAAAAh4/gk-gBPFf-wU/s1600/The%2BBox.jpg" },
+    { desc: "third item", image: "http://1.bp.blogspot.com/-H3xFUgDh8wU/Vj7h8O2xdqI/AAAAAAAAAh4/gk-gBPFf-wU/s1600/The%2BBox.jpg" },
+    { desc: "forth item", image: "http://1.bp.blogspot.com/-H3xFUgDh8wU/Vj7h8O2xdqI/AAAAAAAAAh4/gk-gBPFf-wU/s1600/The%2BBox.jpg" }
+  ];
+
   constructor() { }
 
   ngOnInit() {
